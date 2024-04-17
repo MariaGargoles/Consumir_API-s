19e67c80efa423091ee1d28289db7d0613087268
María Gárgoles <mariagargoles@live.com>
2024-04-17 15:59:33 +0200

    sin acabar

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 //1. Consumir (fetch) la API de usuarios 
 //https://fakerapi.it/api/v1/users?_quantity=20&_gender=male 
 
-
-fetch('https://fakerapi.it/api/v1/persons?_quantity=10&_gender=male&_birthday_start=2005-01-01')
-  .then((response) => {
+const data =  [];
+const request = fetch('https://fakerapi.it/api/v1/persons?_quantity=10').then(
+    (response) => {
     if (response.ok) {
-      response
-        .json()
-        .then(data => console.log(data))
-        .catch((error) => alert("Problemas con la info de la API"));
-    } else {
-      throw new Error('La respuesta no fue exitosa');
+      return response.json();
+      jsonData.push(data)
+      
+
     }
   })
-  .catch((error) => alert("Error en la API"));
+  .then((jsonData) => {
+    
+    const container = document.getElementById("root");
+
+
+    function addPerson(data) {
+        
+        data.forEach(person => {
+          const pElement = document.createElement("p");
+          pElement.innerText = `Nombre: ${person.firstname}, Apellidos: ${person.lastname}, Email: ${person.email}, Telefono: ${person.phone}, Cumpleaños: ${person.birthday}, Direccion: ${person.address}`;
+          container.appendChild(pElement);
+          
+        });
+        console.log(jsonData)
+      }
+  
+    
+      addPerson();
+  })
+  .catch((error) => {
+    alert("Error en la API" + error);
+  });
+
 
 //2. Crear las clases de Persona y Usuarios, 
 //teniendo en cuenta que tienen propiedades comunes
