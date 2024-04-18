7c52e0ff7f2b81c22ae37f8d7aa97213ef39ff1f
María Gárgoles <mariagargoles@live.com>
2024-04-18 12:06:42 +0200

    fix part 1 ex

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 //1. Consumir (fetch) la API de usuarios 
 //https://fakerapi.it/api/v1/users?_quantity=20&_gender=male 
 
-const data =  [];
-const request = fetch('https://fakerapi.it/api/v1/persons?_quantity=10').then(
-    (response) => {
-    if (response.ok) {
-      return response.json();
-      jsonData.push(data)
-      
 
+const request = fetch('https://fakerapi.it/api/v1/persons?_quantity=10')
+  .then((response) => {
+    if (response.ok) {
+      response.json()
+        .then((jsonData) => {
+          pArray = jsonData.data;
+          console.log(pArray);
+          writeHtml(pArray);
+        })
+        .catch((error) => alert('Error'));
     }
   })
-  .then((jsonData) => {
-    
-    const container = document.getElementById("root");
+  .catch((error) => alert('Error'));
 
+let pArray = [];
 
-    function addPerson(data) {
-        
-        data.forEach(person => {
-          const pElement = document.createElement("p");
-          pElement.innerText = `Nombre: ${person.firstname}, Apellidos: ${person.lastname}, Email: ${person.email}, Telefono: ${person.phone}, Cumpleaños: ${person.birthday}, Direccion: ${person.address}`;
-          container.appendChild(pElement);
-          
-        });
-        console.log(jsonData)
-      }
-  
-    
-      addPerson();
-  })
-  .catch((error) => {
-    alert("Error en la API" + error);
+const root = document.getElementById("root");
+
+const writeHtml = (pArray) => {
+  pArray.forEach(person => {
+    const pElement = document.createElement("p");
+    pElement.innerText = `ID: ${person.id}, Nombre: ${person.firstname}, Apellidos: ${person.lastname}, Email: ${person.email}, Telefono: ${person.phone}, Cumpleaños: ${person.birthday}, Direccion: ${person.address}`;
+    root.appendChild(pElement);
   });
+};
 
+    
+    
+
+
+
+ 
+    
+
+      
+          
+   
 
 //2. Crear las clases de Persona y Usuarios, 
 //teniendo en cuenta que tienen propiedades comunes
